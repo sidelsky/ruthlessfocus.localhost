afd1899ba95bc4e70b63c4b9574ef38236185200
Errol Sidelsky <errol@squie.com>
2018-03-28 15:01:55 +0100

    Not complete

--- public/wp-content/themes/ruthlessfocus/partials/welcome.inc.php
+++ public/wp-content/themes/ruthlessfocus/partials/welcome.inc.php
@@ -9,13 +9,16 @@
 
             $title = 'Welcome to';
             $subtitle = 'The Power of Ruthless Focus';
-            $paragraph_desktop = 'Following each of our meetings over the next 5 weeks, locator pins will be unlocked, allowing you to download our pitch presentations and videos. Scroll down and take a look.';
+            $paragraph_desktop = 'Following each of our meetings over the next 5 weeks, content will be unlocked, allowing you to download our pitch presentations and videos. Scroll down and take a look.';
+            $paragraph_mobile = 'Following each of our meetings over the next 5 weeks, content will be unlocked, allowing you to download our pitch presentations and videos. Scroll down and take a look. <br /><br /><small style="font-size: 12px;">NOTE: For full content please display on your desktop.</small>';
 
             echo '<div class="c-welcome__title-container">';
                 echo '<div data-welcome-message>';
                     echo '<h1 class="c-welcome__title">'. $title .'</h1>';
                     echo '<h2 class="c-welcome__sub-title">'. $subtitle .'</h2>';
                     echo '<p class="c-welcome__para c-welcome__para--desktop">'. $paragraph_desktop .'</p>';
+                    echo '<p class="c-welcome__para c-welcome__para--mobile">'. $paragraph_mobile .'</p>';
+
                 echo '</div>';
             echo '</div>';
         ?>
