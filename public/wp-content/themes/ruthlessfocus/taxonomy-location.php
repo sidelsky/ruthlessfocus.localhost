68a20f9ec8159a68237fdef09f457f7895e1fb6d
Milad Alizadeh <milaadd@gmail.com>
2018-11-22 13:55:41 +0000

    update sub-categories

--- public/wp-content/themes/ruthlessfocus/taxonomy-location.php
+++ public/wp-content/themes/ruthlessfocus/taxonomy-location.php
@@ -4,48 +4,60 @@
 	<section class="u-section">
 		<div class="u-l-container--center" data-in-viewport>
 			<div class="u-l-container u-l-container--row u-l-horizontal-padding u-l-vertical-padding u-l-vertical-padding--bottom background-color--white">
-				
+
 				<div class="c-team-list__header">
 					<div class="c-navigation c-navigation--team-list c-team-list__column">
-						<?php 
+						<?php
 
 							// your taxonomy name
 							$terms = get_terms([
 								'taxonomy' => 'location',
-								'childless' => false,
 								'hide_empty' => false,
 								'parent' => 0
 							]);
 
 							$current_term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+							$isSubcategory = $current_term->parent !== 0;
+							$parentId = $current_term->parent;
 
 							foreach ($terms as $term) {
-								$is_active = $term->count >= 1 ? 'active-term' : '' ;	
-								$is_focused = $current_term->slug == $term->slug ? 'focused-term' : '' ;	  
+								$is_active = $term->count >= 1 ? 'active-term' : '' ;
+
+								if ($isSubcategory) {
+									$current_term = get_term_by( 'id', $parentId, get_query_var( 'taxonomy' ) );
+								}
+
+								$is_focused = $current_term->slug == $term->slug ? 'focused-term' : '' ;
 								echo '<a href="'. get_term_link( $term ) .'" class="c-navigation__item '. $is_focused . ' ' . $is_active . '"><span>' . $term->name . '</span></a>';
 							}
 
+
 						?>
-						
+
 						<!-- START: Sub Navigation -->
 						<div class="c-sub-navigation">
 							<?php
 								//Sub items
 								$taxonomy_name = get_queried_object()->taxonomy;
 								$term_id = get_queried_object_id(); // Get the id of the taxonomy
-								$term_children = get_term_children( $term_id, $taxonomy_name ); // Get the children of said taxonomy
 
-								$current_child_term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+								if ($isSubcategory) {
+									$term_children = get_term_children( $parentId, 'location');
+								} else {
+									$term_children = get_term_children( $term_id, $taxonomy_name ); // Get the children of said taxonomy
+								}
 
-								// Display the children 
+								$current_child_term = get_term_by( 'slug', get_query_var( 'location' ), get_query_var( 'taxonomy' ) );
+
+
+								// Display the children
 								foreach ( $term_children as $child ) {
 									$term = get_term_by( 'id', $child, $taxonomy_name );
-									echo '<a href="' . get_term_link( $term->name, $taxonomy_name ) . '" class="c-sub-navigation__item active-term"><span>' . $term->name . '</span></a>';
+									$is_focused = $current_child_term->slug == $term->slug ? 'focused-term' : '' ;
+									echo '<a href="' . get_term_link( $term->name, $taxonomy_name ) . '" class="c-sub-navigation__item active-term ' . $is_focused . ' "><span>' . $term->name . '</span></a>';
 								}
 							?>
 						</div>
-						<!-- END: Sub Navigation -->
-
 					</div>
 
 					<div class="c-team-list__column">
@@ -53,7 +65,7 @@
 							<img class="c-team-list__logo" src="<?php bloginfo('template_url'); ?>/assets/build/img/logo.png" alt="Mediacom" >
 						</a>
 					</div>
-					
+
 				</div>
 
 				<div class="c-team-list__header">
@@ -63,44 +75,26 @@
 						<h2 class="c-team-list__subtitle">Click below to view team biographies</h2>
 					</div>
 				</div>
-				
+
 				<!-- Start: team memebers -->
 				<div class="c-team-list__person-list">
 					<?php
 
-					// 	$terms = get_terms( 'faqs_cat', array(
-					// 		'orderby'    => 'id',
-					// 		'hide_empty' => 1 // hide categories with no posts
-					// 	) 
-					// );
-					
-					$this_term = get_queried_object();
-
-					$terms = get_terms( $this_term->taxonomy, [
-						'parent' => $this_term->term_id,
-						'orderby' => 'slug',
-						'hide_empty' => false
-					] );
-					
-					foreach( $terms as $term ) {
-
-						// Define the query
-		
 						$query = new WP_Query( [
 							'post_type' => 'team',
 							'tax_query' => [
 								'relation' => 'AND',
 								[
-								'taxonomy' => $this_term->taxonomy,
+								'taxonomy' => $current_term->taxonomy,
 								'field'    => 'slug',
-								'terms'    => array( $term->slug )
+								'terms'    => array( $isSubcategory ? $current_child_term->slug : $current_term->slug )
 								]
 							]
 						] );
-				  
-						// output the category in a heading              
+
+						// output the category in a heading
 						//echo'<h2 class="h2 faq-cat">' . $term->name . '</h2>';
-				  
+
 							 // Start the post loop
 							 while ( $query->have_posts() ) : $query->the_post(); ?>
 
@@ -113,16 +107,15 @@
 										</h3>
 									</div>
 								</a>
-				  
+
 							 <?php endwhile;
-				  
+
 						// use reset postdata to restore the original query
 						wp_reset_postdata();
-					}
 						?>
 				</div>
 
-			</div>	
+			</div>
 		</div>
 	</section>
 
